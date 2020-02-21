51f92eb618bc249cf09340c72d8227aee1506e43
Alessandro Oliveira <alessandrooliveira@MacBook-Air-de-Alessandro.local>
2020-02-21 16:02:43 -0300

    ˜merge_com_develop˜

---
+++ ios/build/usecargoapp/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/BVLinearGradient.build/Objects-normal/x86_64/BVLinearGradient-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/alessandrooliveira/workspace/usecargoapp/ios/Pods/Target\ Support\ Files/BVLinearGradient/BVLinearGradient-dummy.m \
+  /Users/alessandrooliveira/workspace/usecargoapp/ios/Pods/Target\ Support\ Files/BVLinearGradient/BVLinearGradient-prefix.pch
