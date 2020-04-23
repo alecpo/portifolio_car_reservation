d79846a92d04efa65b96b5e4c19ba934e1e4f478
Alessandro Oliveira <alessandrooliveira@MacBook-Air-de-Alessandro.local>
2020-04-23 18:53:24 -0300

    n/a

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -135,7 +135,7 @@ const LoginScreen = ({ navigation }) => {
 
 const StyledLinearGradient = styled(LinearGradient)`
   flex: 1;
-  padding-top: ${SPACING.regular};
+  padding-top: ${SPACING.regular}px;
   padding-horizontal: ${SPACING.smallPlus}px;
 `;
 
